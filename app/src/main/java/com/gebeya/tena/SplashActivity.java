b6e3c0288195a4bc3ee2d650c827cdf7ff32b822
melat123 <mel9dagnu@gmail.com>
2021-08-14 14:57:38 +0300

    first commit

---
+++ app/src/main/java/com/gebeya/tena/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.gebeya.tena;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
